9cd8d6b693ed22991bda3f0a6fcccaa3db4bc5e6
Jignesh Makwana <makwana.jignesh@gmail.com>
2023-03-22 10:46:10 +0800

    Added code for multipel env

--- scripts/stack-start.sh
+++ scripts/stack-start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# start stack app1
+# Start-dev
 docker-compose -f ./docker-compose-cicd-app1-dev.yml up -d
+
+# Start-sit
+docker-compose -f ./docker-compose-cicd-app1-sit.yml up -d
+
+# Start-uat
+docker-compose -f ./docker-compose-cicd-app1-uat.yml up -d
+
+# Start-prod
+docker-compose -f ./docker-compose-cicd-app1-prod.yml up -d
