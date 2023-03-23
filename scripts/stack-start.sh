5d7637ebbd35614f840d89b1643c9d1762a3d324
devang704 <devangsybca00@gmail.com>
2023-03-23 22:43:16 +0530

    Update Compose file

--- scripts/stack-start.sh
+++ scripts/stack-start.sh
@@ -4,10 +4,10 @@
 docker-compose -f ./docker-compose-cicd-app1-dev.yml up -d
 
 # Start-sit
-docker-compose -f ./docker-compose-cicd-app1-sit.yml up -d
+# docker-compose -f ./docker-compose-cicd-app1-sit.yml up -d
 
 # Start-uat
-docker-compose -f ./docker-compose-cicd-app1-uat.yml up -d
+# docker-compose -f ./docker-compose-cicd-app1-uat.yml up -d
 
 # Start-prod
-docker-compose -f ./docker-compose-cicd-app1-prod.yml up -d
+# docker-compose -f ./docker-compose-cicd-app1-prod.yml up -d
